1566db7b84be7f0158dbd922d6e74fd85db52d6b
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-03-09 19:38:51 +0200

    Install SignalR Install HangFire

--- PersonalStockTrader.Common/GlobalConstants.cs
+++ PersonalStockTrader.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "AspNetCoreTemplate";
+        public const string SystemName = "PersonalStockTrader";
 
         public const string AdministratorRoleName = "Administrator";
     }
