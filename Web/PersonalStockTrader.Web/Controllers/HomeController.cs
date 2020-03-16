8accd54fc70719d0540708cdc836f9c9134c40b0
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-03-16 13:04:13 +0200

    Submit before adding admin dashboard template

--- Web/PersonalStockTrader.Web/Controllers/HomeController.cs
+++ Web/PersonalStockTrader.Web/Controllers/HomeController.cs
@@ -11,12 +11,7 @@
         {
             return this.View();
         }
-
-        public IActionResult Privacy()
-        {
-            return this.View();
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
