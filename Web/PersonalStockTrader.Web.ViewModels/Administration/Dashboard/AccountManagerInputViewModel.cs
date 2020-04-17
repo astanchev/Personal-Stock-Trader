ec0efbe1ab1356f7d068354c798a2934df6e22f9
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-04-17 19:10:30 +0300

    Few changes

--- Web/PersonalStockTrader.Web.ViewModels/Administration/Dashboard/AccountManagerInputViewModel.cs
+++ Web/PersonalStockTrader.Web.ViewModels/Administration/Dashboard/AccountManagerInputViewModel.cs
@@ -8,6 +8,7 @@
     public class AccountManagerInputViewModel : IMapTo<ApplicationUser>, IMapFrom<ApplicationUser>
     {
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "The {0} must have only letters and numbers.")]
         [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
         [Display(Name = "Username")]
         public string Username { get; set; }
