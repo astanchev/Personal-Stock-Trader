1f6130a68488e6502ef692a6d37c8d6b711d0e3b
Атанас Станчев <mr_Stanchev@hotmail.com>
2020-04-18 18:40:28 +0300

    Add regex validation for username

--- Web/PersonalStockTrader.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web/PersonalStockTrader.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -51,6 +51,7 @@
         public class InputModel
         {
             [Required]
+            [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "The {0} must have only letters and numbers.")]
             [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [Display(Name = "Username")]
             public string Username { get; set; }
